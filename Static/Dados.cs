ec1a92271b9b43e9f4952eed1f18d36f62f77976
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2022-07-01 09:26:00 -0300

    Praticando This, construtor e sobrecarga

---
+++ Static/Dados.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Static {
+    internal class Produto {
+
+        public string Nome;
+        public string Cargo;
+        public int Idade;
+        public double Salario;
+
+
+ 
+        public Produto() {
+            Idade = 0;
+        }
+
+        public Produto(string nome, string cargo, double salario):this() {
+            Nome = nome;
+            Cargo = cargo;
+            Salario = salario;
+        }
+
+        public Produto(string nome, string cargo,int idade ,double salario):this(nome, cargo, salario) { 
+            Idade = idade;
+        }
+        
+        override public string ToString() {
+            return $"O novo funcionário se chama {Nome}, tem {Idade} anos..." +
+                $" Irá ocupar o cargo de {Cargo}" +
+                $" com o salário de {Salario.ToString("f2", CultureInfo.InvariantCulture)}R$ reais";
+        }
+
+
+    }
+}
---
+++ Static/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Static {
+    class Program {
+
+         static void Main(string[] args) {
+
+            
+
+            Console.WriteLine("CADASTRO DE NOVO COLABORADOR: ");
+            Console.WriteLine();
+            Console.Write("VOCÊ QUER CADASTRAR UM NOVO COLABORADOR?");
+            Console.Write("Sim ou Não?: ");
+            string resposta = Console.ReadLine();
+
+            
+
+            if (resposta == "Sim") {
+
+                Console.WriteLine();
+                Console.WriteLine("Vamos lá, preencha os campos");
+                Console.WriteLine();
+                Console.Write("Digite o nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Digite a idade: ");
+                int idade = int.Parse(Console.ReadLine());
+                Console.Write("Digite o cargo: ");
+                string cargo = Console.ReadLine();
+                Console.Write("Digite o salário: ");
+                double salario = double.Parse(Console.ReadLine());
+                Produto pessoa = new Produto(nome, cargo, idade, salario);
+                Console.WriteLine("");
+                Console.WriteLine(pessoa);
+                
+
+            }
+            else {
+                Console.WriteLine();
+                Console.WriteLine("APERTE A TECLA ENTER PARA ENCERRAR O PROGRAMA");
+            }
+            
+        }
+
+    }
+}
